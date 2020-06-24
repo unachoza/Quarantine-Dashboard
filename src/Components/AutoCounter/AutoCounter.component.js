6391a8cab2607a7d51280fbc1cf563c27197fbe2
arianna choza <unachoza@gmail.com>
2020-06-24 01:53:38 -0400

    psuedo code for building counters

--- src/Components/AutoCounter/AutoCounter.component.js
+++ src/Components/AutoCounter/AutoCounter.component.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const AutoCounter = () => {
+  const addAutoCounter = () => {
+    //show form to make autocounter
+    //title of count
+    //starting count
+    //count increments
+    //update count
+    //submit button
+    //render all those things
+  };
   return (
     <div className="AutoCounter__container">
       AutoCounter
+      <button onClick={(e) => addAutoCounter(e)}>Create AutoCounter</button>
     </div>
   );
 };
