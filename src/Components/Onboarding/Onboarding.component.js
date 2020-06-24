a07ffdc7b2e193ccf6408308a3137eae12f8e786
arianna choza <unachoza@gmail.com>
2020-06-24 01:50:52 -0400

    adding buttons for components:

--- src/Components/Onboarding/Onboarding.component.js
+++ src/Components/Onboarding/Onboarding.component.js
@@ -1,18 +1,20 @@
 import React from 'react';
-
+const typesOfStats = ['Goals', 'Countdowns', 'Calendars', 'News'];
 const Onboarding = () => {
+  const handleClick = (e) => {
+    console.log(e.target, 'was clicked');
+  };
   return (
     <div className="Onboarding__container">
       <strong>Keep track of your time, keep track for your sanity, keep track of your mondays</strong>
       <section>
         Add Components that are meaningful to your dashboard
-        <div className="onboarding__title"></div>
-        <ul>
-          <li>Goals</li>
-          <li>Countdowns</li>
-          <li>Calendars</li>
-          <li>News</li>
-        </ul>
+        <div className="onboarding__title">Add Components </div>
+        {typesOfStats.map((stat, i) => (
+          <button key={i} onClick={(e) => handleClick(e)}>
+            {stat}
+          </button>
+        ))}
       </section>
     </div>
   );
