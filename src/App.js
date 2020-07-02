ba9de24d93868de2ee424c7fdb16c798005921fc
arianna choza <unachoza@gmail.com>
2020-07-02 12:12:32 -0400

    added user constants

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import UserDashboard from 'Pages/UserDashboard/UserDashboard.pages.js';
 import BuildDashboard from 'Pages/BuildDashboard/BuildDashboard.pages.js';
 import MiniDrawer from 'Components/LeftSlideDrawer/LeftSlideDrawer.component.js';
 import './App.css';
+import { USER } from 'Constants/User';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="app-container">
-        <div style={{ fontSize: '60px', textAlign: 'center' }}>Quarantine Dashboard</div>
+        <div style={{ fontSize: '60px', textAlign: 'center', marginTop: '60px' }}>{USER.name}'s Dashboard</div>
         <MiniDrawer />
         <BuildDashboard />
         <div className="pages"></div>
