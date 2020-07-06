6a30247156335b742476368a006a32ed99016a95
arianna choza <unachoza@gmail.com>
2020-07-05 23:11:56 -0400

    chaning list title

--- src/Components/TodoList/Layout.component.js
+++ src/Components/TodoList/Layout.component.js
@@ -1,16 +1,26 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import { AppBar, Toolbar, Typography, Paper } from '@material-ui/core';
-const Layout = memo((props) => (
-  <Paper
-    elevation={0}
-    style={{ padding: 0, margin: 0, backgroundColor: '#fafafa', border: 'solid', width: 'fit-content' }}
-  >
-    <AppBar color="primary" position="static" style={{ height: 64 }}>
-      <Toolbar style={{ height: 64 }}>
-        <Typography color="inherit">Create a todo List</Typography>
-      </Toolbar>
-    </AppBar>
-    {props.children}
-  </Paper>
-));
+
+const Layout = memo((props) => {
+  const [title, setTitle] = useState('Create a List');
+  const titleList = (e) => {
+    console.log('clicked this title', e);
+    setTitle(e.target.value);
+  };
+  return (
+    <Paper
+      elevation={0}
+      style={{ padding: 0, margin: 0, backgroundColor: '#fafafa', border: 'solid', width: 'fit-content' }}
+    >
+      <AppBar color="primary" position="static" style={{ height: 64 }}>
+        <Toolbar style={{ height: 64 }}>
+          <Typography color="inherit" onClick={titleList}>
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {props.children}
+    </Paper>
+  );
+});
 export default Layout;
